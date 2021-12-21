6c75827bf57e494ce9619740ef50672cc4abc002
Harshavardhan M <harsha7vardhan@gmail.com>
2021-12-21 11:26:17 +0530

    added amplify

--- lib/main.dart
+++ lib/main.dart
@@ -5,24 +5,26 @@ import 'package:flutter/material.dart';
 import 'screens/register_screen.dart';
 
 void main() {
-  runApp(diaryapp());
+  runApp(const DiaryApp());
 }
 
-class diaryapp extends StatelessWidget {
+class DiaryApp extends StatelessWidget {
+  const DiaryApp({Key? key}) : super(key: key);
+
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home:SplashScreen(),
     );
   }
 }
 
 
-
-
 class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
