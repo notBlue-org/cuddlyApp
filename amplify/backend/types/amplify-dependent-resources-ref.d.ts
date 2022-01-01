5f509c10e1fef1168f3a458fa0d9f03a05d77af3
Aditya Anil <aditya8anil@gmail.com>
2022-01-01 16:54:22 +0530

    [cleanup] Clean the code

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "agrisoftapps": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
