fcc1036f6e16cf44507e6fb3114994ff9318eb8e
Harshavardhan M <harsha7vardhan@gmail.com>
2022-01-02 22:27:46 +0530

    deleted a,plify

---
+++ amplifyconfiguration.dart
@@ -0,0 +1,17 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "api": {
+        "plugins": {
+            "awsAPIPlugin": {
+                "agrisoftapps": {
+                    "endpointType": "GraphQL",
+                    "endpoint": "https://vgf5qfy2bfcanm2ipysn64keuu.appsync-api.ap-south-1.amazonaws.com/graphql",
+                    "region": "ap-south-1",
+                    "authorizationType": "API_KEY",
+                    "apiKey": "da2-ypcojqzax5at7jme2usfsee5c4"
+                }
+            }
+        }
+    }
+}''';
--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,4 +1,5 @@
 import 'package:diaryapp/screens/login.dart';
+import 'package:diaryapp/utils/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -24,23 +25,72 @@ class _HomeScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'HomePage',
+      title: 'Profile Page',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Home Page'),
+          title: const Text('Profile Page'),
         ),
         body: Center(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'NAME: ${_currentUser.displayName}',
+              'Name: ${_currentUser.displayName}',
               style: Theme.of(context).textTheme.bodyText1,
             ),
             Text(
-              'EMAIL: ${_currentUser.email}',
+              'Email: ${_currentUser.email}',
               style: Theme.of(context).textTheme.bodyText1,
             ),
+            const SizedBox(height: 16.0),
+            _currentUser.emailVerified
+                ? Text(
+                    'Email verified',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1!
+                        .copyWith(color: Colors.green),
+                  )
+                : Text(
+                    'Email not verified',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1!
+                        .copyWith(color: Colors.red),
+                  ),
+            const SizedBox(height: 16.0),
+            _isSendingVerification
+                ? const CircularProgressIndicator()
+                : Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () async {
+                          setState(() {
+                            _isSendingVerification = true;
+                          });
+                          await _currentUser.sendEmailVerification();
+                          setState(() {
+                            _isSendingVerification = false;
+                          });
+                        },
+                        child: const Text('Verify email'),
+                      ),
+                      const SizedBox(width: 8.0),
+                      IconButton(
+                        icon: const Icon(Icons.refresh),
+                        onPressed: () async {
+                          User? user = await FireAuth.refreshUser(_currentUser);
+
+                          if (user != null) {
+                            setState(() {
+                              _currentUser = user;
+                            });
+                          }
+                        },
+                      ),
+                    ],
+                  ),
             ElevatedButton(
                 onPressed: () async {
                   await FirebaseAuth.instance.signOut();
