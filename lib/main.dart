473a0cfa0bad21c2ac0249f1fbf9a92cf41fc970
Aditya Anil <aditya8anil@gmail.com>
2022-01-02 11:11:59 +0530

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,16 @@
-import 'dart:async';
-
-import 'package:diaryapp/screens/splashScreen.dart';
+import 'package:diaryapp/screens/SplashScreen/splashScreen.dart';
 import 'package:flutter/material.dart';
-
 
 void main() {
   runApp(diaryapp());
 }
 
 class diaryapp extends StatelessWidget {
-
   // This widgets is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:SplashScreen(),
+      home: SplashScreen(),
     );
   }
 }
-
-
-
-
-
-
-
-
--- lib/screens/SplashScreen/splashScreen.dart
+++ lib/screens/SplashScreen/splashScreen.dart
@@ -1,36 +1,37 @@
+// ignore_for_file: file_names
 
 import 'dart:async';
-import 'package:diaryapp/screens/home/widget/home.dart';
+import 'package:diaryapp/screens/home/home.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
   @override
   void initState() {
     Timer(
         const Duration(
           seconds: 2,
-        ),(){
-      Navigator.pushReplacement(context,MaterialPageRoute(
-        builder:(context)=>HomePage(),
-      ));
-    }
-    );
-    // TODO: implement initState
+        ), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(),
+          ));
+    });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body:Center(
-          child: Image.asset('assets/images/Notblue.png'),
-        )
-    );
+        body: Center(
+      child: Image.asset('assets/images/Notblue.png'),
+    ));
   }
 }
