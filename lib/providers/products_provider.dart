e07ef0103a00934530a3e578881451ec712a973a
Aditya Anil <aditya8anil@gmail.com>
2022-01-10 11:27:35 +0530

    filter bar added

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -55,14 +55,60 @@ class Products with ChangeNotifier {
       imageUrl:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
     ),
+    Product(
+      id: 'p5',
+      brand: 'company2',
+      title: 'Red Shirt',
+      description: '1 box 100 items',
+      price: 29.00,
+      imageUrl:
+          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    ),
+    Product(
+      id: 'p6',
+      brand: 'company3',
+      title: 'Trousers',
+      description: '1 box 100 items.',
+      price: 59.00,
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+    ),
+    Product(
+      id: 'p7',
+      brand: 'company4',
+      title: 'Yellow Scarf',
+      description: '1 box 50 items',
+      price: 19.00,
+      imageUrl:
+          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+    ),
+    Product(
+      id: 'p8',
+      brand: 'company5',
+      title: 'A Pan',
+      description: '1 box 80 items.',
+      price: 49.00,
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+    ),
   ];
 
   List<Product> get items {
     return [..._items];
   }
 
-  void addProduct() {
-    // _items.add(value);
+  var _filterItems;
+  Products() {
+    _filterItems =
+        _items.where((element) => element.brand == 'company1').toList();
+  }
+
+  void filter({String brand = 'company1'}) {
+    _filterItems = _items.where((element) => element.brand == brand).toList();
     notifyListeners();
+  }
+
+  List<Product> get filterItems {
+    return [..._filterItems];
   }
 }
--- lib/screens/app/order_pages/order_page.dart
+++ lib/screens/app/order_pages/order_page.dart
@@ -1,6 +1,6 @@
-
 import 'package:diaryapp/providers/cart.dart';
 import 'package:diaryapp/providers/products_provider.dart';
+import 'package:diaryapp/widgets/filter_widget.dart';
 import 'package:diaryapp/widgets/nav_drawer.dart';
 import 'package:diaryapp/widgets/product_item.dart';
 import 'package:flutter/material.dart';
@@ -31,24 +31,74 @@ class OrderPage extends StatelessWidget {
   }
 }
 
-class ProductGrid extends StatelessWidget {
+class ProductGrid extends StatefulWidget {
   const ProductGrid({Key? key}) : super(key: key);
 
   @override
+  State<ProductGrid> createState() => _ProductGridState();
+}
+
+class _ProductGridState extends State<ProductGrid> {
+  int selectedIndex = 0;
+  List categories = [
+    'company1',
+    'company2',
+    'company3',
+    'company4',
+    'company5',
+  ];
+  @override
   Widget build(BuildContext context) {
     final productData = Provider.of<Products>(context);
-    final productList = productData.items;
-    return ListView.builder(
-      itemBuilder: (ctx, index) => ProductItem(
-        id: productList[index].id,
-        title: productList[index].title,
-        imageUrl: productList[index].imageUrl,
-        price: productList[index].price,
-        description: productList[index].description,
+    var productList = productData.filterItems;
+    return Column(children: [
+      Container(
+        margin: const EdgeInsets.all(8),
+        height: 40,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: categories.length,
+          itemBuilder: (context, index) => GestureDetector(
+            onTap: () => {
+              setState(() {
+                selectedIndex = index;
+                productData.filter(brand: categories[index]);
+              })
+            },
+            child: Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(
+                left: 10,
+                // At end item it add extra 20 right  padding
+                right: index == categories.length - 1 ? 10 : 0,
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                color: index == selectedIndex ? Colors.pink : Colors.lightBlue,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Text(
+                categories[index],
+                style: const TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+        ),
       ),
-      padding: const EdgeInsets.all(10),
-      itemCount: productList.length,
-      scrollDirection: Axis.vertical,
-    );
+      Expanded(
+        child: ListView.builder(
+          itemBuilder: (ctx, index) => ProductItem(
+            id: productList[index].id,
+            title: productList[index].title,
+            imageUrl: productList[index].imageUrl,
+            price: productList[index].price,
+            description: productList[index].description,
+          ),
+          padding: const EdgeInsets.all(10),
+          itemCount: productList.length,
+          scrollDirection: Axis.vertical,
+        ),
+      ),
+    ]);
   }
 }
