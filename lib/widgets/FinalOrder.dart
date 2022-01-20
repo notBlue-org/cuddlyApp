e70cd3e1db42f46965bc2d25064a792e12cf6d22
Aditya Anil <aditya8anil@gmail.com>
2022-01-20 23:44:36 +0530

    working product

--- lib/widgets/FinalOrder.dart
+++ lib/widgets/FinalOrder.dart
@@ -1,21 +1,20 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
+import 'package:provider/single_child_widget.dart';
 import 'package:razorpay_flutter/razorpay_flutter.dart';
 import './order_button.dart';
 
 class FinalOrder extends StatefulWidget {
-
-
-  FinalOrder(this.Totalamount,this.addUser) ;
+  FinalOrder(this.Totalamount, this.addUser) {}
   Future<void> addUser;
   double Totalamount;
   @override
   _FinalOrderState createState() => _FinalOrderState();
-
 }
 
 class _FinalOrderState extends State<FinalOrder> {
   late Razorpay razorpay;
-
 
   @override
   void initState() {
@@ -25,59 +24,39 @@ class _FinalOrderState extends State<FinalOrder> {
     razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, handlerPaymentSuccess);
     razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, handlerErrorFailure);
     razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, handlerExternalWallet);
+    openCheckout();
   }
 
-  void handlerPaymentSuccess(Future<void> addUser){
-   addUser ;
-
+  void handlerPaymentSuccess(Future<void> addUser) {
+    addUser;
   }
 
-  void handlerErrorFailure(){
+  void handlerErrorFailure() {
     print("Payment success");
-
   }
 
-  void handlerExternalWallet(){
+  void handlerExternalWallet() {
     print("External Wallet");
-
   }
 
-  void openCheckout()async{
+  void openCheckout() async {
     var options = {
-      "key" : "rzp_test_QgshDg09IHVGJn",
-      "amount" :widget.Totalamount*100,
-      "name" : "Agrisoft ",
-      "description" : "Payment for the Diary Prodcucts You have Purchased For",
-
-      "prefill" : {
-        "contact" : "9567190733",
-        "email" : "notBlue@gmail.com"
-      },
-
+      "key": "rzp_test_QgshDg09IHVGJn",
+      "amount": widget.Totalamount * 100,
+      "name": "Agrisoft ",
+      "description": "Payment for the Diary Prodcucts You have Purchased For",
+      "prefill": {"contact": "9567190733", "email": "notBlue@gmail.com"},
     };
 
-    try{
+    try {
       razorpay.open(options);
-    }catch(e){
+    } catch (e) {
       print(e.toString());
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Final Order Page')),
-      body:
-      Column(
-        children: [
-          SizedBox(height: 12) ,
-          ElevatedButton(onPressed: (){
-            openCheckout();
-          },
-            child: Text("paynow"),)
-        ],
-      ),
-
-    );
+    return Scaffold();
   }
 }
