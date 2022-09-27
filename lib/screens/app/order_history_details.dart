21e2fb19e411c020867bd26dc4a346877e099262
Aditya Anil <aditya8anil@gmail.com>
2022-09-27 09:31:22 +0530

    Order histroy page ui done

--- lib/screens/app/order_history_details.dart
+++ lib/screens/app/order_history_details.dart
@@ -3,6 +3,8 @@ import 'package:diaryapp/widgets/cust_appbar.dart';
 import 'package:diaryapp/widgets/nav_drawer.dart';
 import 'package:flutter/material.dart';
 
+import '../../constants/colors.dart';
+
 class OrderHistoryDetailsPage extends StatelessWidget {
   final objectData;
   const OrderHistoryDetailsPage(this.objectData, {Key? key}) : super(key: key);
@@ -26,6 +28,43 @@ class OrderHistoryDetailsPageBody extends StatefulWidget {
       OrderHistoryDetailsPageBodyState();
 }
 
+_getField(width, label, value) {
+  return [
+    Container(
+      padding: EdgeInsets.fromLTRB(0.1 * width, 0, 0, 0),
+      alignment: Alignment.centerLeft,
+      child: Text(
+        label,
+        textAlign: TextAlign.left,
+        style:
+            const TextStyle(color: kButtonColor, fontWeight: FontWeight.bold),
+      ),
+    ),
+    const SizedBox(
+      height: 5,
+    ),
+    Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16.0),
+            color: Colors.white,
+            boxShadow: const [
+              BoxShadow(
+                color: Color.fromRGBO(13, 21, 129, 0.03),
+                blurRadius: 100.0,
+                offset: Offset(0, 10.0),
+                spreadRadius: 2,
+              ),
+            ]),
+        height: 50,
+        width: 0.8 * width,
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          value,
+          style: const TextStyle(color: kButtonColor),
+        ))
+  ];
+}
+
 class OrderHistoryDetailsPageBodyState
     extends State<OrderHistoryDetailsPageBody> {
   @override
@@ -39,109 +78,229 @@ class OrderHistoryDetailsPageBodyState
       SingleChildScrollView(
         child: Container(
             padding: const EdgeInsets.only(left: 10.0),
-            alignment: Alignment.topLeft,
-            decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Color.fromRGBO(13, 21, 129, 0.03),
-                    blurRadius: 100.0,
-                    offset: Offset(0, 10.0),
-                    spreadRadius: 2,
-                  ),
-                ]),
             child: Column(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: width * 0.5,
-                      child: const Text(
-                        "Order ID",
-                        style: TextStyle(fontWeight: FontWeight.bold),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16.0),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color.fromRGBO(13, 21, 129, 0.03),
+                          blurRadius: 100.0,
+                          offset: Offset(0, 10.0),
+                          spreadRadius: 2,
+                        ),
+                      ]),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Row(
+                          children: [
+                            Container(
+                                margin: const EdgeInsets.only(left: 10.0),
+                                child: const Text(
+                                  'Order Id',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                )),
+                            const Spacer(),
+                            Container(
+                                margin: const EdgeInsets.only(right: 10.0),
+                                child: Text(widget.objectData.id.toString())),
+                          ],
+                        ),
                       ),
-                    ),
-                    Text(widget.objectData.id),
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Row(
+                          children: [
+                            Container(
+                                margin: const EdgeInsets.only(left: 10.0),
+                                child: const Text(
+                                  'OTP',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                )),
+                            const Spacer(),
+                            Container(
+                                margin: const EdgeInsets.only(right: 10.0),
+                                child: Text(widget.objectData.otp.toString())),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Row(
+                          children: [
+                            Container(
+                                margin: const EdgeInsets.only(left: 10.0),
+                                child: const Text(
+                                  'Payment Type',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                )),
+                            const Spacer(),
+                            Container(
+                                margin: const EdgeInsets.only(right: 10.0),
+                                child: Text(
+                                    widget.objectData.paymentType.toString())),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Row(
+                          children: [
+                            Container(
+                                margin: const EdgeInsets.only(left: 10.0),
+                                child: const Text(
+                                  'Total Price',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                )),
+                            const Spacer(),
+                            Container(
+                                margin: const EdgeInsets.only(right: 10.0),
+                                child: Text(
+                                    widget.objectData.totalPrice.toString())),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                        child: Row(
+                          children: [
+                            Container(
+                                margin: const EdgeInsets.only(left: 10.0),
+                                child: const Text(
+                                  'Status',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                )),
+                            const Spacer(),
+                            Container(
+                                margin: const EdgeInsets.only(right: 10.0),
+                                child:
+                                    Text(widget.objectData.status.toString())),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      // const Divider(
+                      //   color: Colors.black,
+                      // ),
+                    ],
+                  ),
                 ),
-                const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: width * 0.5,
-                      child: const Text(
-                        "OTP",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Product Details',
+                    style: TextStyle(
+                      color: kButtonColor,
+                      fontFamily: 'Poppins',
+                      fontSize: 20,
+                      fontWeight: FontWeight.w900,
                     ),
-                    Text(widget.objectData.otp),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: width * 0.5,
-                      child: const Text(
-                        "Payment Type",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Text(widget.objectData.paymentType),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: width * 0.5,
-                      child: const Text(
-                        "Total Price",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Text(widget.objectData.totalPrice.toString()),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: width * 0.5,
-                      child: const Text(
-                        "Status",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Text(widget.objectData.status),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: width * 0.4,
-                      child: const Text(
-                        "Product List",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
                 ListView.builder(
+                  padding: EdgeInsets.zero,
                   shrinkWrap: true,
                   itemBuilder: (ctx, i) =>
                       OrderHistoryProductItem(widget.objectData.productList[i]),
                   itemCount: widget.objectData.productList.length,
                   scrollDirection: Axis.vertical,
                 ),
+
+                // Container(
+                //     decoration: BoxDecoration(
+                //         borderRadius: BorderRadius.circular(16.0),
+                //         color: Colors.white,
+                //         boxShadow: const [
+                //           BoxShadow(
+                //             color: Color.fromRGBO(13, 21, 129, 0.03),
+                //             blurRadius: 100.0,
+                //             offset: Offset(0, 10.0),
+                //             spreadRadius: 2,
+                //           ),
+                //         ]),
+                //     height: 50,
+                //     width: 0.8 * width,
+                //     padding: const EdgeInsets.all(15),
+                //     child: Text(
+                //       widget.objectData.id,
+                //       style: const TextStyle(color: kButtonColor),
+                //     )),
+
+                // const SizedBox(height: 10),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.start,
+                //   children: [
+                //     SizedBox(
+                //       width: width * 0.5,
+                //       child: const Text(
+                //         "OTP",
+                //         style: TextStyle(fontWeight: FontWeight.bold),
+                //       ),
+                //     ),
+                //     Text(widget.objectData.otp),
+                //   ],
+                // ),
+                // const SizedBox(height: 10),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.start,
+                //   children: [
+                //     SizedBox(
+                //       width: width * 0.5,
+                //       child: const Text(
+                //         "Payment Type",
+                //         style: TextStyle(fontWeight: FontWeight.bold),
+                //       ),
+                //     ),
+                //     Text(widget.objectData.paymentType),
+                //   ],
+                // ),
+                // const SizedBox(height: 10),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.start,
+                //   children: [
+                //     SizedBox(
+                //       width: width * 0.5,
+                //       child: const Text(
+                //         "Total Price",
+                //         style: TextStyle(fontWeight: FontWeight.bold),
+                //       ),
+                //     ),
+                //     Text(widget.objectData.totalPrice.toString()),
+                //   ],
+                // ),
+                // const SizedBox(height: 10),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.start,
+                //   children: [
+                //     SizedBox(
+                //       width: width * 0.5,
+                //       child: const Text(
+                //         "Status",
+                //         style: TextStyle(fontWeight: FontWeight.bold),
+                //       ),
+                //     ),
+                //     Text(widget.objectData.status),
+                //   ],
+                // ),
+                // const SizedBox(height: 20),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.start,
+                //   children: [
+                //     SizedBox(
+                //       width: width * 0.4,
+                //       child: const Text(
+                //         "Product List",
+                //         style: TextStyle(fontWeight: FontWeight.bold),
+                //       ),
+                //     ),
+                //   ],
+                // ),
               ],
             )),
       )
@@ -159,49 +318,74 @@ class OrderHistoryProductItem extends StatelessWidget {
     double width = MediaQuery.of(context).size.width;
 
     return Container(
-      padding: const EdgeInsets.only(left: 10.0),
-      child: Column(children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(
-              width: width * 0.3,
-              child: const Text(
-                "Name",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
+      margin: const EdgeInsets.only(left: 5.0, right: 10.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10.0),
+        child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
             ),
-            Text(productDetailsMap["Name"]),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(
-              width: width * 0.3,
-              child: const Text(
-                "Quantity",
-                style: TextStyle(fontWeight: FontWeight.bold),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+              child: Column(
+                children: [
+                  Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                    Container(
+                      padding: const EdgeInsets.only(left: 15.0),
+                      child: const Text(
+                        'Name',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const Spacer(),
+                    Container(
+                        margin: const EdgeInsets.only(right: 15.0),
+                        child: Text(
+                          productDetailsMap['Name'],
+                          style: const TextStyle(
+                              fontSize: 15, fontStyle: FontStyle.italic),
+                        )),
+                  ]),
+                  Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                    Container(
+                      padding: const EdgeInsets.only(left: 15.0),
+                      child: const Opacity(
+                        opacity: 0.5,
+                        child: Text(
+                          'Quantity',
+                          style: TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    Container(
+                        margin: const EdgeInsets.only(right: 15.0),
+                        child: Text(productDetailsMap['Quantity'].toString())),
+                  ]),
+                  Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                    Container(
+                      padding: const EdgeInsets.only(left: 15.0),
+                      child: const Opacity(
+                        opacity: 0.5,
+                        child: Text(
+                          'Price',
+                          style: TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    Container(
+                        margin: const EdgeInsets.only(right: 15.0),
+                        child: Text(
+                            productDetailsMap['Price'].toInt().toString())),
+                  ]),
+                ],
               ),
-            ),
-            Text(productDetailsMap["Quantity"].toString()),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(
-              width: width * 0.3,
-              child: const Text(
-                "Price",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ),
-            Text(productDetailsMap["Price"].toString()),
-          ],
-        ),
-        const SizedBox(height: 10),
-      ]),
+            )),
+      ),
     );
   }
 }
